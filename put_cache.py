614ca1c70b6b86ec04e84bf885ba91125b1949c4
lhf <lhfbc@163.com>
2019-01-23 20:36:36 +0800

    add 'put_cache' tests.

---
+++ put_cache.py
@@ -0,0 +1,36 @@
+import time
+import commands
+import os
+
+txn_num = 500
+
+# 1. get current txn count
+tx_count = commands.getoutput("grep \"totalTransactions =\" ./node1/iri.log  | tail -n 1 | awk '{print $25}'")
+print "tx_count = ", tx_count
+
+# 2. send batch of transactions
+for i in range(txn_num):
+    ret = os.system('curl -X POST   http://127.0.0.1:5000/put_cache -H \'Content-Type: application/json\' -H \'cache-control: no-cache\' -d \'{"type": "trans","from_address": "i","to_address": "j","amount": "6"}\'')
+    if ret == 0:
+        print "Send command successfully ", i
+    else:
+        print "Send command failed ", i
+        exit(-1)
+
+# 3. after some seconds, get current txn count and judge
+counter = 0
+while 1:
+    time.sleep(3)
+    new_tx_count = commands.getoutput("grep \"totalTransactions =\" ./node1/iri.log  | tail -n 1 | awk '{print $25}'")
+    print "new_tx_count = ", new_tx_count
+    if int(new_tx_count) == int(tx_count) + txn_num:
+        print "IOTA transaction count added successfully"
+        exit(0)
+    else:
+        counter += 1
+        if counter < 50:
+            print "waiting for IOTA transaction count..."
+        else:
+            print "Error! transaction number added failed!"
+            exit(-1)
+
