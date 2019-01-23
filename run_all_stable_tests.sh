abdbce7bebd04dce2beebe00fd1246b90baa1b51
lhf <lhfbc@163.com>
2019-01-23 20:45:11 +0800

    add milestone and add test.

--- run_all_stable_tests.sh
+++ run_all_stable_tests.sh
@@ -15,6 +15,8 @@ bash 2_start_and_stop_a_node_known_database-testnet.sh $VER
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 bash 3_create_a_transaction_on_Node_A_and_find_it_in_Node_B.sh $VER
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-bash 4_send_batches_of_transactions_without_database-testnet.sh $VER
+bash 4_send_transactions_without_batch_without_database-testnet.sh $VER
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+bash 5_send_transactions_with_batch_without_database-testnet.sh $VER
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 echo "finished running tests successfully"
