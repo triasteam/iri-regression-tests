bd68f93e56fcf06c5957e0d8651f7eb32558062f
lhf <45928776+lhfbc@users.noreply.github.com>
2019-03-08 11:12:12 +0800

    add failover test: txn count. (#21)

--- common.py
+++ common.py
@@ -1,6 +1,29 @@
 import os
 import time
 import commands
+import subprocess
+import signal
+
+# start iota
+def start_iota():
+    status = os.system('java -jar node1/iri-1.5.5.jar -p 14700 -u 14700 -t 14701 -n udp://localhost:14699 udp://localhost:14701 --testnet --testnet-no-coo-validation --snapshot=./Snapshot.txt --mwm 1 --walk-validator "NULL" --ledger-validator "NULL" --max-peers 40 --remote --ipfs-txns false --batch-txns > node1/iri.log 2>&1 &')
+    #print status, output
+    if status == 0:
+        print "Start IOTA successfully"
+    else:
+        print "Start IOTA failed: "
+        exit(-1)
+
+    time.sleep(8)
+
+# stop iota
+def stop_iota():
+    p = subprocess.Popen(['ps', '-aux'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    for line in out.splitlines():
+        if 'java -jar node1/iri' in line:
+            pid = int(line.split()[1])
+            os.kill(pid, signal.SIGKILL)
 
 # send milestone
 def send_milestone():
@@ -44,7 +67,7 @@ def start_cli(enable_ipfs=True, enable_batch=False, enable_compression=False):
 
     cur_dir = os.getcwd()
     os.chdir("../../scripts/iota_api")
-    os.system("python ./app.py &> /tmp/app.log &")
+    os.system("python ./app.py > /tmp/app.log 2>&1 &")
     os.chdir(cur_dir)
 
     time.sleep(3)
