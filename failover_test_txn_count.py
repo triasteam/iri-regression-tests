28453ebed450b3d5106388073d4a1c5fae0baa7a
lhfbc <lhf_bc@163.com>
2019-03-11 11:42:19 +0800

    modification on sleepting duration and printing.

--- failover_test_txn_count.py
+++ failover_test_txn_count.py
@@ -1,5 +1,7 @@
 import common, time, os
 
+print("running test failover_test_txn_count")
+
 os.chdir('iri')
 
 print("\nStarting cli...\n")
@@ -13,23 +15,22 @@ common.send_milestone()
 
 print("\nStarting to send txs...\n")
 common.put_file(20)
+time.sleep(20)
 
-time.sleep(10)
 
 txn_count = common.get_transactions_count()
 print("\nold tx_count = %s\n" % txn_count)
 
 print("\nStopping iota...\n")
 common.stop_iota()
-time.sleep(10)
+time.sleep(5)
 
 print("\nRestarting iota...\n")
 common.start_iota()
-
-time.sleep(10)
+time.sleep(20)
 
 new_txn_count = common.get_transactions_count()
-print("\nnew tx_count = %s\n" % txn_count)
+print("\nnew tx_count = %s\n" % new_txn_count)
 
 print("\nStopping iota and cli...\n")
 common.stop_iota()
