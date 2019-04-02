6df58e02a6f66737901d4b483fea156b19152bdd
lhfbc <lhf_bc@163.com>
2019-04-02 17:40:17 +0800

    modify the scripts.

--- 9_send_utxo_transactions_two_nodes_without_batch.sh
+++ 9_send_utxo_transactions_two_nodes_without_batch.sh
@@ -54,17 +54,19 @@ total_1=0
 total_2=0
 declare -A balances_1
 declare -A balances_2
-WRONG="false"
+FLAG="false"
 for account in {a..z} {A..Z}
 do
     balances_1[$account]=$(curl -s -X GET http://127.0.0.1:5000/get_balance -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d "{\"account\": \"$account\"}")
     balances_2[$account]=$(curl -s -X GET http://127.0.0.1:6000/get_balance -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d "{\"account\": \"$account\"}")
     total_1=$((total_1+balances_1[$account]))
     total_2=$((total_2+balances_2[$account]))
-    if [ ${balance_1[$account]} != ${balance_2[$account]} ] ; then
-        WRONG="true"
-        break
+    if [ ${balances_1[$account]} != ${balances_2[$account]} ] ; then
+        echo "Balances of" $account "in node1 and node2 are different:" ${balances_1[$account]} -- ${balances_2[$account]}
+        FLAG="true"
+        #break
     fi
+    echo "total balances " $total_1 -- $total_2
 done
 
 # stop iota and cli
@@ -73,10 +75,14 @@ ps -aux | grep "[j]ava -jar iri" | awk '{print $2}' | xargs kill -9
 mv iri/scripts/iota_api/conf.bak iri/scripts/iota_api/conf
 
 # check the total balance
-if [ ${WRONG} == "false" ] && [ ${total_1} -eq "1000000000" ] && [ ${total_2} -eq "1000000000" ];
+if [ ${FLAG} == "false" ] && [ ${total_1} -eq "1000000000" ] && [ ${total_2} -eq "1000000000" ];
 then
-    echo "UTXO with double spending is OK"
+    echo
+    echo "UTXO with double spending is OK!"
 else
-    echo "Wrong total balance: ", ${total_1}, ${total_2}
+    echo "Wrong!"
+    echo "    FLAG = " $FLAG
+    echo "    total of node1" ${total_1}
+    echo "    total of node2" ${total_2}
     exit -1
 fi
