af20b6ccb6a6a15aa0c88989591cc1480e1f5245
lhf <45928776+lhfbc@users.noreply.github.com>
2019-04-12 10:12:01 +0800

    [fix #29] add regresstion test for get_balance (#30)
    * add regresstion test for get_balance
    
    * modify the scripts.
    
    * add the new test into the main script.
    
    * cat the log file after failure.
    
    * add regresstion test for getting balance with put_cache
    
    * change log path.
    
    * codacy.
    
    * codacy
    
    * modify .travis.yml
    
    * change log path.
    
    * fix errors.
    
    * fix ci errors.
    
    * add batch flag.
    
    * add debug info
    
    * add debug info.
    
    * modify debug info.
    
    * add debug info.
    
    * change http port
    
    * modify start_iota
    
    * increase waiting time
    
    * increase waiting time.
    
    * copy
    
    * remove debug log

--- common.py
+++ common.py
@@ -6,27 +6,28 @@ import signal
 import sh
 
 # start iota
-def start_iota():
-    status = os.system('java -jar node1/iri-1.5.5.jar -p 14700 -u 14700 -t 14701 -n udp://localhost:14699 udp://localhost:14701 --testnet --testnet-no-coo-validation --snapshot=./Snapshot.txt --mwm 1 --walk-validator "NULL" --ledger-validator "NULL" --max-peers 40 --remote --ipfs-txns false --batch-txns > node1/iri.log 2>&1 &')
-    #print status, output
-    if status != 0:
-        print "Start IOTA failed: "
-        exit(-1)
+def start_iota(node=1):
+    jar_file = "node%d/iri-1.5.5.jar" % (node)
+    log_file = "node%d/iri.log" % (node)
+    _status = sh.java("-jar", jar_file, "-p", "14700", "-u", "14700", "-t", "14701",
+                     "-n", "udp://localhost:14699", "udp://localhost:14701", "--testnet", "--testnet-no-coo-validation",
+                     "--snapshot=./Snapshot.txt", "--mwm", "1", "--walk-validator", "NULL", "--ledger-validator", "NULL",
+                     "--max-peers", "40", "--remote", "--ipfs-txns", "false", "--batch-txns", _out=log_file, _bg=True)
 
     time.sleep(8)
 
 
 # stop iota
-def stop_iota():
+def stop_iota(node=1):
     p = subprocess.Popen(['ps', '-aux'], stdout=subprocess.PIPE)
     out, err = p.communicate()
     for line in out.splitlines():
-        if 'java -jar node1/iri' in line:
+        if ('java -jar node%d/iri' % node) in line:
             pid = int(line.split()[1])
             os.kill(pid, signal.SIGKILL)
 
 
-def start_cli(enable_ipfs=True, enable_batch=False, enable_compression=False):
+def start_cli(enable_ipfs=True, enable_batch=False, enable_compression=False, node=1):
     # change config
     cur_dir = os.getcwd()
 
@@ -57,16 +58,22 @@ def start_cli(enable_ipfs=True, enable_batch=False, enable_compression=False):
         f.write(file_data)
 
     os.chdir("scripts/iota_api")
-    os.system("python ./app.py > /tmp/app.log 2>&1 &")
+    log_file = "%s/node%d/app.log" % (cur_dir, node)
+    sh.python("./app.py", _out=log_file, _bg=True)
     os.chdir(cur_dir)
 
     time.sleep(3)
 
 
 def stop_cli():
-    os.system('ps -aux | grep "[p]ython ./app" | awk \'{print $2}\' | xargs kill -9')
-    sh.mv("scripts/iota_api/conf.bak", "scripts/iota_api/conf")
+    p = subprocess.Popen(['ps', '-aux'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    for line in out.splitlines():
+        if ('app.py') in line:
+            pid = int(line.split()[1])
+            os.kill(pid, signal.SIGKILL)
 
+    sh.mv("scripts/iota_api/conf.bak", "scripts/iota_api/conf")
 
 # send transactions one by one
 def put_file(txn_num=1):
@@ -87,8 +94,9 @@ def put_cache(txn_num=1):
 
 
 # get current txn count
-def get_transactions_count():
-    tx_count = commands.getoutput("grep -a \"totalTransactions =\" ./node1/iri.log  | tail -n 1 | awk '{print $25}'")
+def get_transactions_count(node=1):
+    log_file = "node%d/iri.log" % node
+    tx_count = sh.awk(sh.tail(sh.grep("-a", "totalTransactions =", log_file), "-n", "1"), '{print $25}')
     return tx_count
 
 
