71424f7355d2cc648dfcff2666d03efff4c37b48
lihaifeng111 <1105586992@qq.com>
2019-06-17 17:23:43 +0800

    [fix #10]modify_11_vue_interface_test

--- 11_vue_interface_test.sh
+++ 11_vue_interface_test.sh
@@ -16,7 +16,7 @@ java -jar iri/target/iri-1.5.5.jar --testnet --mwm 1 --walk-validator "NULL" --l
                         --db-log-path "./db1.log" --max-peers 40 --remote \
                         --enable-streaming-graph --entrypoint-selector-algorithm "KATZ" --tip-sel-algo "CONFLUX" \
                         --ipfs-txns false --batch-txns --weight-calculation-algorithm "IN_MEM" \
-                        &>  iri/node1/iri.log &
+                        &>  iri/node1/iri1.log &
 sleep 1
 echo "iota install success"
 # run main.go
@@ -29,37 +29,57 @@ echo "go install success"
 sleep 2
 
 # send request AddNode
-#
-ip_random1=100
-ip_random2=10
-for ((i=1;i<=3;i++));
+attester_1=192.168.130.101
+attestee_1=192.168.130.110
+score_1=1
+touch result.txt
+curl -s -X POST http://127.0.0.1:8000/AddNode -H 'Content-Type:application/json' -H 'cache-control: no-cache' -d "{\"Attester\":\"${attester_1}\",\"Attestee\":\"${attestee_1}\",\"Score\":\"${score_1}\"}"
+
+#send request QueryNodes
+result1=$(curl -s -X POST http://127.0.0.1:8000/QueryNodes -H 'Content-Type:application/json' -H 'cache-control: no-cache' -d "{\"period\":1,\"numRank\":100}")
+
+echo $result1
+echo $result1 > result.txt
+
+#send request QueryNodes
+attester_2=192.168.130.110
+attestee_2=192.168.130.112
+score_2=2
+curl -s -X POST http://127.0.0.1:8000/AddNode -H 'Content-Type:application/json' -H 'cache-control: no-cache' -d "{\"Attester\":\"${attester_2}\",\"Attestee\":\"${attestee_2}\",\"Score\":\"${score_2}\"}"
+
+#send request QueryNodes
+result2=$(curl -s -X POST http://127.0.0.1:8000/QueryNodes -H 'Content-Type:application/json' -H 'cache-control: no-cache' -d "{\"period\":1,\"numRank\":100}")
+echo $result2
+echo $result2 >> result.txt
+array=$(awk -F '[:,"]' '{$1="";print $0}' result.txt)
+rm -rf result.txt
+
+# determine whether to include
+function contains()
+{
+for i in ${array[@]}
 do
-    score=$i
-    attester=$((ip_random1 + i))
-    attestee=$((i * ip_random2 + ip_random1))
-    result=$(curl -s -X POST http://127.0.0.1:8000/AddNode -H 'Content-Type:application/json' -H 'cache-control: no-cache' -d "{\"Attester\":\"192.168.130.${attester}\",\"Attestee\":\"192.168.130.${attestee}\",\"Score\":\"${score}\"}")
-    code1=$(echo $result | sed -e 's/[{}]/''/g' | sed s/\"//g | awk -v RS=',' -F: '$1=="Code"{print $2}')
-    if [ $code1 -eq 1 ];
+    if [ $i == $1 ]
     then
-        echo "AddNode test${i} success"
-    else
-        echo "Wrong!"
-        echo $result
+        echo "$1 query success"
+        break
+    elif [ $i == score ]
+        then
+        echo "Wrong"
+        echo "$1 query failed"
+        ps -aux | grep "[g]o-build" | awk '{print $2}' | xargs kill -9
+        ps -aux | grep "[j]ava -jar iri" | awk '{print $2}' | xargs kill -9
+        cd ../../../../
+        rm -rf iri/node1/iri1.log
         exit -1
     fi
 done
+}
+contains $attester_1
+contains $attestee_1
+contains $attester_2
+contains $attestee_2
 
-# send request QueryNodes
-nodes=$(curl -s -X POST http://127.0.0.1:8000/QueryNodes -H 'Content-Type:application/json' -H 'cache-control: no-cache' -d "{\"period\":1,\"numRank\":100}")
-code2=$(echo $nodes | sed -e 's/[{}]/''/g' | sed s/\"//g | awk -v RS=',' -F: '$1=="Code"{print $2}')
-    if [ $code2 -eq 1 ];
-    then
-        echo "QueryNodes test success"
-    else
-        echo "Wrong!"
-        echo $nodes
-        exit -1
-    fi
 # stop iota and cli
 ps -aux | grep "[g]o-build" | awk '{print $2}' | xargs kill -9
 ps -aux | grep "[j]ava -jar iri" | awk '{print $2}' | xargs kill -9
@@ -67,7 +87,7 @@ ps -aux | grep "[j]ava -jar iri" | awk '{print $2}' | xargs kill -9
 sleep 1
 # return iri dir
 cd ../../../../
-
+rm -rf iri/node1/iri1.log
 echo "run over"
 
 
