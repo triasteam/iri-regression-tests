ebc6dfdd8e1d7602037e06e714f9e40a4942c2a1
lhfbc <lhf_bc@163.com>
2019-06-19 10:55:21 +0800

    enable batch

--- 12_genesis_forward_and_stress_test.sh
+++ 12_genesis_forward_and_stress_test.sh
@@ -18,7 +18,7 @@ java -jar iri/target/iri-1.5.5.jar --testnet --mwm 1 --walk-validator "NULL" --l
                         --udp-receiver-port $((PORT1-100)) --tcp-receiver-port $((PORT1-100)) --db-path "./db1" \
                         --db-log-path "./db1.log" --neighbors "tcp://localhost:$((PORT2-100))" --max-peers 40 --remote \
                         --enable-streaming-graph --entrypoint-selector-algorithm "KATZ" --tip-sel-algo "CONFLUX" \
-                        --ipfs-txns false --batch-txns --weight-calculation-algorithm "IN_MEM" \
+                        --ipfs-txns false --batch-txns true --weight-calculation-algorithm "IN_MEM" \
                         --ancestor-forward-enable true --ancestor-create-frequency 1000 \
                         &>  iri/node1/iri.log &
 
@@ -28,7 +28,7 @@ java -jar iri/target/iri-1.5.5.jar --testnet --mwm 1 --walk-validator "NULL" --l
                         --udp-receiver-port $((PORT2-100)) --tcp-receiver-port $((PORT2-100)) --db-path "./db2" \
                         --db-log-path "./db2.log" --neighbors "tcp://localhost:$((PORT1-100))" --max-peers 40 --remote \
                         --enable-streaming-graph --entrypoint-selector-algorithm "KATZ" --tip-sel-algo "CONFLUX" \
-                        --ipfs-txns false --batch-txns --weight-calculation-algorithm "IN_MEM" \
+                        --ipfs-txns false --batch-txns true --weight-calculation-algorithm "IN_MEM" \
                         &>  iri/node2/iri.log &
 
 sleep 1
@@ -36,33 +36,59 @@ sleep 1
 # start cli
 cd iri/scripts/iota_api
 cp conf conf.bak
-cp ../examples/two_nodes/cli_conf_two_nodes_1 conf
-sed -i "s/enableBatching = False/enableBatching = True/g" conf
+cp ../examples/two_nodes_batch/cli_conf_two_nodes_1 conf
 python app.py &> ${DIR}/iri/node1/app.log  &
 
 sleep 10
 
-cp ../examples/two_nodes/cli_conf_two_nodes_2 conf
-sed -i "s/enableBatching = False/enableBatching = True/g" conf
+cp ../examples/two_nodes_batch/cli_conf_two_nodes_2 conf
 python app.py &> ${DIR}/iri/node2/app.log  &
 
 cd ${DIR}
 
-#sleep 5
 
 # start nginx
 sed -i -e "s/LOCAL_IP/localhost/g" iri/scripts/iota_perf/nginx.conf
 sudo cp iri/scripts/iota_perf/nginx.conf /etc/nginx/
 sudo nginx -s reload
 
-sleep 10
+sleep 60 
 
 # start jmeter
-#
+
 # It will check the result in 'run_perf.sh'
-#
+
 cd iri/scripts/iota_perf
-./run_perf.sh
+python data_generate.py 10000
+
+# run ramp up
+sed -e 's/NUM_CALL/500/g'       \
+    -e 's/NUM_THREAD/1/g'       \
+    -e 's/PORT/5000/g'          \
+    -e 's/DATA/data/g'          \
+    -e 's/ACTION/put_file/g'   PerformanceTestDAG2TM_TPS.jmx > PerformanceTest.jmx
+jmeter -n -t PerformanceTest.jmx
+
+sleep 10
+
+# run perf with multi-threading
+sed -e 's/NUM_CALL/5000/g'  \
+    -e 's/NUM_THREAD/2/g'   \
+    -e 's/PORT/8080/g'      \
+    -e 's/DATA/data1/g'     \
+    -e 's/ACTION/put_cache/g' PerformanceTestDAG2TM_TPS.jmx > PerformanceTest1.jmx
+jmeter -n -t PerformanceTest1.jmx
+
+sleep 30
+
+# check balance
+./check_result.sh
+
+# check order
+curl -s -X GET http://127.0.0.1:5000/get_total_order -H 'Content-Type: application/json' -H 'cache-control: no-cache' > order1
+curl -s -X GET http://127.0.0.1:6000/get_total_order -H 'Content-Type: application/json' -H 'cache-control: no-cache' > order2
+diff order1 order2
+
 cd ${DIR}
 
 # stop iota and cli
