a78350564d970a2187f1e5601a88ab37d36ee18d
lhfbc <lhf_bc@163.com>
2019-06-21 14:31:55 +0800

    modify script

--- 12_genesis_forward_and_stress_test.sh
+++ 12_genesis_forward_and_stress_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "running test 12 testnet - genensis forward"
+echo "running test 12 testnet - genensis forward with stress test"
 
 set -e
 
@@ -36,11 +36,13 @@ sleep 1
 cd iri/scripts/iota_api
 cp conf conf.bak
 cp ../examples/two_nodes_batch/cli_conf_two_nodes_1 conf
+sed -i -e "s/enableBatching = False/enableBatching = True/g" conf
 python app.py &> ${DIR}/iri/node1/app.log  &
 
 sleep 10
 
 cp ../examples/two_nodes_batch/cli_conf_two_nodes_2 conf
+sed -i -e "s/enableBatching = False/enableBatching = True/g" conf
 python app.py &> ${DIR}/iri/node2/app.log  &
 
 cd ${DIR}
@@ -51,42 +53,12 @@ sed -i -e "s/LOCAL_IP/localhost/g" iri/scripts/iota_perf/nginx.conf
 sudo cp iri/scripts/iota_perf/nginx.conf /etc/nginx/
 sudo nginx -s reload
 
-sleep 60 
+sleep 60
 
 # start jmeter
-
 # It will check the result in 'run_perf.sh'
-
 cd iri/scripts/iota_perf
-python data_generate.py 10000
-
-# run ramp up
-sed -e 's/NUM_CALL/500/g'       \
-    -e 's/NUM_THREAD/1/g'       \
-    -e 's/PORT/5000/g'          \
-    -e 's/DATA/data/g'          \
-    -e 's/ACTION/put_file/g'   PerformanceTestDAG2TM_TPS.jmx > PerformanceTest.jmx
-jmeter -n -t PerformanceTest.jmx
-
-sleep 10
-
-# run perf with multi-threading
-sed -e 's/NUM_CALL/5000/g'  \
-    -e 's/NUM_THREAD/2/g'   \
-    -e 's/PORT/8080/g'      \
-    -e 's/DATA/data1/g'     \
-    -e 's/ACTION/put_cache/g' PerformanceTestDAG2TM_TPS.jmx > PerformanceTest1.jmx
-jmeter -n -t PerformanceTest1.jmx
-
-sleep 30
-
-# check balance
-./check_result.sh
-
-# check order
-curl -s -X GET http://127.0.0.1:5000/get_total_order -H 'Content-Type: application/json' -H 'cache-control: no-cache' > order1
-curl -s -X GET http://127.0.0.1:6000/get_total_order -H 'Content-Type: application/json' -H 'cache-control: no-cache' > order2
-diff order1 order2
+./run_perf.sh
 
 cd ${DIR}
 
@@ -94,4 +66,3 @@ cd ${DIR}
 ps -aux | grep "[p]ython app.py" | awk '{print $2}' | xargs kill -9
 ps -aux | grep "[j]ava -jar iri" | awk '{print $2}' | xargs kill -9
 mv iri/scripts/iota_api/conf.bak iri/scripts/iota_api/conf
-
